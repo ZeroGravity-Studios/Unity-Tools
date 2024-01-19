1766bce5c889b276b29af3cdaeb761de38318d89
Mokarran <9244335+Sphyrna-029@users.noreply.github.com>
2024-01-18 22:30:52 -0500

    Update ZGAFModTools.cs

--- ZGAFModTools.cs
+++ ZGAFModTools.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class SettingsMenu : MonoBehaviour
 {
-    [MenuItem("ZGAF Mod Tools//Lighting/Apply Lights To Empties")]
+    [MenuItem("ZGAF Mod Tools//Lighting/Apply Point Lights To Empties")]
     static void ApplyLightToEmpty()
     {
         var lightObject = Selection.activeGameObject;
@@ -17,7 +17,75 @@ public class SettingsMenu : MonoBehaviour
 
             if (child.name.Contains("Lights"))
             {
+                Light lightComp = child.AddComponent<Light>();
+                lightComp.type = LightType.Point;
+
+                Debug.Log("Added Point light to " + child.name);
+            }
+
+        }
+
+    }
+
+    [MenuItem("ZGAF Mod Tools//Lighting/Apply Spot Lights To Empties")]
+    static void ApplySpotLightToEmpty()
+    {
+        var lightObject = Selection.activeGameObject;
+
+        for (int i = 0; i < lightObject.transform.childCount; i++)
+        {
+            GameObject child = lightObject.transform.GetChild(i).gameObject;
+
+            if (child.name.Contains("Lights"))
+            {
                 child.AddComponent<Light>();
+                Light lightComp = child.AddComponent<Light>();
+                lightComp.type = LightType.Spot;
+
+                Debug.Log("Added light to " + child.name);
+            }
+
+        }
+
+    }
+
+    [MenuItem("ZGAF Mod Tools//Lighting/Apply Directional Lights To Empties")]
+    static void ApplyDirectionalLightToEmpty()
+    {
+        var lightObject = Selection.activeGameObject;
+
+        for (int i = 0; i < lightObject.transform.childCount; i++)
+        {
+            GameObject child = lightObject.transform.GetChild(i).gameObject;
+
+            if (child.name.Contains("Lights"))
+            {
+                child.AddComponent<Light>();
+                Light lightComp = child.AddComponent<Light>();
+                lightComp.type = LightType.Directional;
+
+                Debug.Log("Added light to " + child.name);
+            }
+
+        }
+
+    }
+
+    [MenuItem("ZGAF Mod Tools//Lighting/Apply Area Lights To Empties")]
+    static void ApplyAreaLightToEmpty()
+    {
+        var lightObject = Selection.activeGameObject;
+
+        for (int i = 0; i < lightObject.transform.childCount; i++)
+        {
+            GameObject child = lightObject.transform.GetChild(i).gameObject;
+
+            if (child.name.Contains("Lights"))
+            {
+                child.AddComponent<Light>();
+                Light lightComp = child.AddComponent<Light>();
+                lightComp.type = LightType.Area;
+
                 Debug.Log("Added light to " + child.name);
             }
 
